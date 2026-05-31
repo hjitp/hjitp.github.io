4f860d768e778129ba1a58782a71ffdfba70a245
hjitp <nicolas.spycher@posteo.de>
2026-05-31 15:55:50 +0200

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,12 @@
+document.getElementById('sidebar').innerHTML = `
+  <a class="header-image" href="index.html">
+    <img src="Header200pxbroad.jpg" alt="hjitp.ch">
+  </a>
+  <nav>
+    <a href="filter.html">Filter</a>
+    <a href="aengstli.html">ÄNGSTLI</a>
+    <span class="empty-line">&nbsp;</span>
+    <a href="about.html">About</a>
+    <a href="newsletter.html">Newsletter</a>
+  </nav>
+`;
