9db2d0aa0d9de3a6efd6f435486a0d219754799c
hjitp <nicolas.spycher@posteo.de>
2026-06-24 15:32:24 +0200

    Remove duplicate 'Drama Online' link from sidebar

--- sidebar.js
+++ sidebar.js
@@ -3,9 +3,9 @@ document.getElementById('sidebar').innerHTML = `
     <img src="Header200pxbroad.jpg" alt="hjitp.ch">
   </a>
   <nav>
+    <a href="dramaonline.html">Drama Online</a>
     <a href="filter.html">Filter</a>
     <a href="aengstli.html">ÄNGSTLI</a>
-    <a href="dramaonline.html">Drama Online</a>
     <span class="empty-line">&nbsp;</span>
     <a href="about.html">About</a>
     <a href="newsletter.html">Newsletter</a>
